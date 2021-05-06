d71e369a8471f514cb407eb26ae67c13e507d37d
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-05-06 07:53:11 -0300

    💥 NEW: Usando SSG no Next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
 import Head from 'next/head';
 
@@ -44,7 +44,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1Io3wzFG23k2C6HtK6CtkIeY');
 
   const product = {
@@ -59,5 +59,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
